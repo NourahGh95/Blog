d5b5e8035457ffd719a1e031c6f1139c1007f07e
Nourah <nourah.abghanim@hotmail.com>
2022-12-05 16:54:41 +0300

    Initial Commit

---
+++ blog/blogApp.swift
@@ -0,0 +1,17 @@
+//
+//  blogApp.swift
+//  blog
+//
+//  Created by Nourah on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct blogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
